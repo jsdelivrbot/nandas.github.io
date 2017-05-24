3fbf2c9e06f0b120b4e0a21698c06bb47fe75053
nandas <nandakumararaja@gmail.com>
2017-05-24 02:53:52 -0400

    making port heroku compatible

--- app.js
+++ app.js
@@ -47,5 +47,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 //module.exports = app;
